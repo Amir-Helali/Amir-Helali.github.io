2f2b71462c699f6c8a3851b55b289177564f3c4d
Amir Helali <54836705+Amir-Helali@users.noreply.github.com>
2022-01-23 15:16:33 -0500

    Updates

---
+++ precache-manifest.f44da97e22b07f7f58fddd55e1bb2e1b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ffb68e34b6ac4ea5776df8ff242282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74fe3b3d46f4f2f4190f",
+    "url": "/static/css/2.016a4bc0.chunk.css"
+  },
+  {
+    "revision": "b2e4eb7068ddfa500a66",
+    "url": "/static/css/main.6c336171.chunk.css"
+  },
+  {
+    "revision": "74fe3b3d46f4f2f4190f",
+    "url": "/static/js/2.5764cdb1.chunk.js"
+  },
+  {
+    "revision": "9f06fd6f843f39b33f97",
+    "url": "/static/js/3.06b3e647.chunk.js"
+  },
+  {
+    "revision": "b2e4eb7068ddfa500a66",
+    "url": "/static/js/main.375b940d.chunk.js"
+  },
+  {
+    "revision": "4ec9f2f7ae8603f40151",
+    "url": "/static/js/runtime~main.08e9c602.js"
+  },
+  {
+    "revision": "64d4bed62ed1c4ff1929f6473b342345",
+    "url": "/static/media/aboutMe.64d4bed6.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
